dbf70b5cc831a46d4d7685018b3efd5ee5a20d05
jaeho <wogh001004@gmail.com>
2025-01-17 11:34:14 +0900

    서비스 구현

--- src/main/java/com/myfruit/pms/mapper/ItemMapper.java
+++ src/main/java/com/myfruit/pms/mapper/ItemMapper.java
@@ -3,8 +3,16 @@ package com.myfruit.pms.mapper;
 import com.myfruit.pms.dto.ItemDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Optional;
+
 @Mapper
 public interface ItemMapper {
+    //ItemDTO insertItem(ItemDTO itemDTO);
+    //int insertItem(ItemDTO itemDTO);
     void insertItem(ItemDTO itemDTO);
-    ItemDTO getItemById(int id);
+    Optional<ItemDTO> getItemById(int id);
+    List<ItemDTO> getItems();
+    void updateItem(ItemDTO itemDTO);
+    void deleteItem(int id);
 }
--- src/main/java/com/myfruit/pms/service/ItemService.java
+++ src/main/java/com/myfruit/pms/service/ItemService.java
@@ -5,14 +5,36 @@ import com.myfruit.pms.mapper.ItemMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ItemService {
 
     @Autowired
     private ItemMapper itemMapper;
 
+    public void createItem(ItemDTO itemDTO) {
+        itemMapper.insertItem(itemDTO);
+    }
+
     public ItemDTO getItem(int id) {
-        return itemMapper.getItemById(id);
+        // NullException
+        return itemMapper.getItemById(id).orElseThrow(
+                () -> new IllegalArgumentException("파일을 찾을 수 없습니다.")
+        );
+    }
+
+    public List<ItemDTO> getItems() {
+        return itemMapper.getItems();
+    }
+
+    public void modify(ItemDTO itemDTO) {
+        itemMapper.updateItem(itemDTO);
+    }
+
+    public void remove(int id) {
+        itemMapper.deleteItem(id);
     }
 }
 
